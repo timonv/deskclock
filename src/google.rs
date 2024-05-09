4fe309d313e37aa2fd1b41092d02c98abb554e97
Timon Vonk <timonv@gmail.com>
2024-05-09 11:58:44 +0200

    feat: itsaclock

--- src/google.rs
+++ src/google.rs
@@ -1,13 +1,9 @@
-// extern crate google_calendar3 as calendar3;
-// extern crate hyper;
-// extern crate hyper_rustls;
 use anyhow::Result;
 use chrono::Duration;
-use google_calendar3::api::{Channel, Event, EventDateTime, Events};
+use google_calendar3::api::{EventDateTime, Events};
 use google_calendar3::hyper::client::HttpConnector;
 use google_calendar3::hyper_rustls::HttpsConnector;
-use google_calendar3::Error;
-use google_calendar3::{chrono, hyper, hyper_rustls, oauth2, CalendarHub, FieldMask};
+use google_calendar3::{chrono, hyper, hyper_rustls, oauth2, CalendarHub};
 use log::info;
 use std::default::Default;
 
@@ -69,18 +65,9 @@ fn extract_date_or_default(date_time: Option<EventDateTime>) -> chrono::DateTime
 impl AsyncCalendar {
     async fn authenticate(&mut self) {
         info!("Authenticating with Google Calendar API");
-        // Get an ApplicationSecret instance by some means. It contains the `client_id` and
-        // `client_secret`, among other things.
-        // let secret: oauth2::ApplicationSecret = Default::default();
-        // Read secret from file
         let secret = oauth2::read_application_secret("credentials.json")
             .await
             .unwrap();
-        // Instantiate the authenticator. It will choose a suitable authentication flow for you,
-        // unless you replace  `None` with the desired Flow.
-        // Provide your own `AuthenticatorDelegate` to adjust the way it operates and get feedback about
-        // what's going on. You probably want to bring in your own `TokenStorage` to persist tokens and
-        // retrieve them from storage.
         let auth = oauth2::InstalledFlowAuthenticator::builder(
             secret,
             oauth2::InstalledFlowReturnMethod::HTTPRedirect,
@@ -104,13 +91,6 @@ impl AsyncCalendar {
     }
 
     async fn get_events(&self) -> Result<Events> {
-        // As the method needs a request, you would usually fill it with the desired information
-        // into the respective structure. Some of the parts shown here might not be applicable !
-        // Values shown here are possibly random and not representative !
-
-        // You can configure optional parameters by calling the respective setters at will, and
-        // execute the final call using `doit()`.
-        // Values shown here are possibly random and not representative !
         let time_min = chrono::Utc::now() - Duration::minutes(10);
         log::warn!(
             "Getting events from Google Calendar API between {} and now",
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
 use std::{error::Error, time::Duration};
 
-use chrono::Timelike;
+use chrono::{DateTime, Local, Timelike};
 use eframe::egui::{self, RichText, WindowLevel};
 use google::GoogleCalendar;
+use log::warn;
 
 mod google;
 mod oauth_browser_delegate;
 
+static GOOGLE_REFRESH_INTERVAL: i64 = 60 * 60; // 1 hour
+                                               //
 fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
 
-    let mut calendar = google::GoogleCalendar::try_new()?;
-    calendar.authenticate();
-
+    #[cfg(not(debug_assertions))]
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
             .with_window_level(WindowLevel::AlwaysOnTop)
@@ -21,95 +22,206 @@ fn main() -> Result<(), Box<dyn Error>> {
         ..Default::default()
     };
 
+    #[cfg(debug_assertions)]
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_window_level(WindowLevel::AlwaysOnTop)
+            .with_inner_size([800.0, 400.0])
+            .with_decorations(false),
+        ..Default::default()
+    };
+
     eframe::run_native(
         "Deskclock",
         options,
-        Box::new(|_cc| Box::new(MyApp::default().with_updated_events(calendar))),
+        Box::new(|_cc| Box::new(MyApp::default())),
     )?;
     Ok(())
 }
 
 struct MyApp {
-    hour: u32,
-    minute: u32,
-    second: u32,
+    current_time: DateTime<Local>,
+    last_event_update: DateTime<Local>,
     events: Vec<google::CalendarEvent>,
+    calendar: GoogleCalendar,
+    current_filter: EventFilter,
+}
+
+#[derive(PartialEq)]
+enum EventFilter {
+    Today,
+    Later,
 }
 
 impl Default for MyApp {
     fn default() -> Self {
-        let time = chrono::offset::Local::now();
+        let current_time = chrono::offset::Local::now();
+        let mut calendar =
+            google::GoogleCalendar::try_new().expect("Failed to create GoogleCalendar");
+        calendar.authenticate();
+        let events = calendar.get_events();
 
         MyApp {
-            hour: time.hour(),
-            minute: time.minute(),
-            second: time.second(),
-            events: Vec::new(),
+            current_time,
+            last_event_update: current_time,
+            calendar,
+            events,
+            current_filter: EventFilter::Today,
         }
     }
 }
 
 impl MyApp {
-    pub fn with_updated_events(mut self, calendar: GoogleCalendar) -> Self {
-        let events = calendar.get_events();
-
-        self.events = events;
-        self
+    fn update_time(&mut self) {
+        self.current_time = chrono::offset::Local::now();
     }
 
-    fn update_time(&mut self) {
-        let time = chrono::offset::Local::now();
-        self.hour = time.hour();
-        self.minute = time.minute();
-        self.second = time.second();
+    fn update_events(&mut self) {
+        // This will block the UI for a while, but it's fine for now
+        // It's just a clock
+        let now = chrono::offset::Local::now();
+        if now
+            .signed_duration_since(self.last_event_update)
+            .num_seconds()
+            > GOOGLE_REFRESH_INTERVAL
+        {
+            warn!("Updating events");
+            self.events = self.calendar.get_events();
+            self.last_event_update = now;
+        }
+    }
+
+    fn render_time(&self, ui: &mut egui::Ui) {
+        ui.label(
+            RichText::new(format!(
+                "{:02}:{:02}",
+                self.current_time.hour(),
+                self.current_time.minute(),
+            ))
+            .monospace()
+            .size(50.0),
+        );
+    }
+
+    fn render_events(&self, ui: &mut egui::Ui) {
+        let today = self.current_time.date_naive();
+        let events = self
+            .events
+            .iter()
+            .filter(|event| match self.current_filter {
+                EventFilter::Today => event.start.date_naive() == today,
+                EventFilter::Later => event.start.date_naive() > today,
+            });
+
+        let mut last_date = today;
+        for event in events {
+            ui.horizontal(|ui| {
+                if EventFilter::Later == self.current_filter {
+                    if last_date != event.start.date_naive() {
+                        ui.label(
+                            RichText::new(format!("{}", event.start.format("%d-%m"))).strong(),
+                        );
+                    } else {
+                        ui.add_space(40.0);
+                    }
+                    last_date = event.start.date_naive();
+                }
+
+                let mut text = RichText::new(format!(
+                    "{} - {}:   {}",
+                    event.start.format("%H:%M"),
+                    event.end.format("%H:%M"),
+                    event.summary
+                ));
+
+                if event.end < self.current_time {
+                    text = text.strikethrough();
+                }
+
+                ui.label(text);
+            });
+        }
+    }
+
+    fn render_next_event(&self, ui: &mut egui::Ui) {
+        if let Some(event) = self
+            .events
+            .iter()
+            .find(|event| event.end > self.current_time || event.start > self.current_time)
+        {
+            let mut text = RichText::new(format!(
+                "{} - {}:   {}",
+                event.start.format("%H:%M"),
+                event.end.format("%H:%M"),
+                event.summary
+            ));
+
+            // About to start
+            if event.start > (self.current_time - chrono::Duration::minutes(10)) {
+                text = text.color(egui::Color32::LIGHT_RED);
+            }
+
+            // Already started
+            if event.start < self.current_time {
+                text = text.color(egui::Color32::LIGHT_GREEN);
+            }
+
+            ui.label(text.strong());
+        }
+    }
+
+    fn render_event_label(&mut self, ui: &mut egui::Ui, filter: EventFilter) {
+        use EventFilter::*;
+
+        let enabled = self.current_filter == filter;
+        let text = RichText::new(match filter {
+            Today => "today",
+            Later => "later",
+        })
+        .size(20.0);
+        if enabled {
+            ui.label(text.strong());
+        } else if ui.label(text).clicked() {
+            self.current_filter = filter;
+        }
     }
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         self.update_time();
+        self.update_events();
 
         egui::CentralPanel::default().show(ctx, |ui| {
             if ui.small_button("close").clicked() {
                 std::process::exit(0);
             };
-            ui.vertical_centered_justified(|ui| {
+            ui.allocate_ui(egui::Vec2::new(800.0, 400.0), |ui| {
                 ui.horizontal_centered(|ui| {
-                    ui.label(
-                        RichText::new(format!(
-                            "{:02}:{:02}:{:02}",
-                            self.hour, self.minute, self.second
-                        ))
-                        .monospace()
-                        .size(50.0),
-                    );
-                    ui.vertical(|ui| {
-                        for event in &self.events {
-                            ui.label(format!(
-                                "{} - {}:   {}",
-                                event.start.format("%d/%m %H:%M"),
-                                event.end.format("%H:%M"),
-                                event.summary
-                            ));
-                        }
+                    ui.allocate_ui(egui::Vec2::new(400.0, 400.0), |ui| {
+                        ui.vertical_centered_justified(|ui| {
+                            self.render_time(ui);
+                            self.render_next_event(ui);
+                        });
+                    });
+                    ui.allocate_ui(egui::Vec2::new(400.0, 400.0), |ui| {
+                        ui.horizontal_centered(|ui| {
+                            ui.vertical(|ui| {
+                                ui.horizontal(|ui| {
+                                    self.render_event_label(ui, EventFilter::Today);
+                                    ui.add_space(10.0);
+                                    self.render_event_label(ui, EventFilter::Later);
+                                });
+                                ui.separator();
+                                ui.add_space(5.0);
+                                self.render_events(ui);
+                            });
+                        });
                     });
                 });
             });
-
-            // ui.heading("My egui Application");
-            // ui.horizontal(|ui| {
-            //     let name_label = ui.label("Your name: ");
-            //     ui.text_edit_singleline(&mut self.name)
-            //         .labelled_by(name_label.id);
-            // });
-            // ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            // if ui.button("Increment").clicked() {
-            //     self.age += 1;
-            // }
-            //
-            // ui.label(format!("Hello '{}', age {}", self.name, self.age));
         });
 
-        ctx.request_repaint_after(Duration::new(1, 0));
+        ctx.request_repaint_after(Duration::new(60, 0));
     }
 }
